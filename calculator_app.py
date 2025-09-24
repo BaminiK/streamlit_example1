d760f9adf5051704dc76a0e6a51bbb242619f704
BaminiK <baminik@gmail.com>
2025-09-24 15:03:01 +0530

    Update calculator_app.py

--- calculator_app.py
+++ calculator_app.py
@@ -9,7 +9,7 @@ st.write("This app connects to a FastAPI calculator service.")
 
 #api_url = "http://localhost:9322"
 
-api_url = "https://api-example-bamini.onrender.com\docs"
+api_url = "https://api-example-bamini.onrender.com"
 
 # Initialize session state to store the calculator display and current operation
 if 'display' not in st.session_state:
